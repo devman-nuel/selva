f7811e983ce04caa082ca59bd3f25879b2350a7f
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-10-18 10:08:55 +0100

    yes

--- src/Components/Bento/Bento.js
+++ src/Components/Bento/Bento.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './Bento.css';
 
 function Bento() {
@@ -9,9 +9,57 @@ function Bento() {
         <p>Manage your events and appointments, set reminders ensuring your schedule stays organized and stress-free</p>
       </div>
 
-      
+      <div className='offer-con'>
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Seamless Scheduling</p>
+              <span>Effortlessly find common availability and schedule meetings seamlessly.</span>
+            </div>
+          </div>
+
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Automated Reminders</p>
+              <span>Stay on top of your commitments with automated reminders and notifications.</span>
+            </div>
+          </div>
+
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Event Management</p>
+              <span>Manage your events with ease, from planning to execution, all in one place.</span>
+            </div>
+          </div>
+          
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Team Collaboration</p>
+              <span>Work together with your team to achieve more with shared calendars and resources.</span>
+            </div>
+          </div>
+          
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Analytics and Insights</p>
+              <span>Gain insights into your schedule and optimize your time management.</span>
+            </div>
+          </div>
+
+          <div className='offer-box'>
+            <div className='offer-visuals'></div>
+            <div className='offer-text'>
+              <p>Analytics and Insights</p>
+              <span>Gain insights into your schedule and optimize your time management.</span>
+            </div>
+          </div>
+        </div>
     </div>
-  )
+  );
 }
 
-export default Bento
+export default Bento;
