965f824197db049a21ece212c946b9f2f05a81d0
emmanuelakharia <107748497+vibeauratech@users.noreply.github.com>
2024-10-19 22:10:44 +0100

    Update Pricing.js

--- src/Components/Pricing/Pricing.js
+++ src/Components/Pricing/Pricing.js
@@ -22,22 +22,22 @@ function Pricing() {
 
                     <div className='pricing-list'>
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Basic customization options</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>No - priority customer support</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Limited playlist functionality</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Real-time syncing across devices</p>
                         </div>
                     </div>
@@ -57,22 +57,22 @@ function Pricing() {
 
                     <div className='pricing-list'>
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_1686552853_ac02z6.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Advanced customization options</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_1686552853_ac02z6.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Dedicated priority customer support</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_1686552853_ac02z6.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Newly-enhanced playlist functionality</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_1686552853_ac02z6.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Cross-device compatibility</p>
                         </div>
                     </div>
@@ -92,22 +92,22 @@ function Pricing() {
 
                     <div className='pricing-list'>
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>All features of the Duo plan</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Dedicated account manager</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Content management features</p>
                         </div>
 
                         <div className='pricing-detail'>
-                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1716711236/Group_2425_x6nltt.jpg' alt=''></img>
+                            <img src='https://res.cloudinary.com/dxnukbo0u/image/upload/v1729371839/icons8-done-24_lqv2in.png' alt=''></img>
                             <p>Enterprise-grade security and compliance</p>
                         </div>
                     </div>
